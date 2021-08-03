80010cbf96e6619d8cac882345571472d66e5d1a
mervelil <71037128+mervelil@users.noreply.github.com>
2021-08-03 20:27:16 +0300

    Add files via upload

---
+++ Card/NewsCard.style.js
@@ -0,0 +1,23 @@
+import {Dimensions, StyleSheet } from "react-native";
+export default StyleSheet.create({
+    container:{
+backgroundColor:'white',
+margin:10,
+borderRadius:10,
+    },
+    image:{height:Dimensions.get('window').height/4,borderTopLeftRadius:10,borderTopRightRadius:10,},
+    title:{
+        fontSize:18,
+        fontWeight:'bold',
+    },
+    description:{
+        marginTop:4,
+        },
+    author:{
+        fontStyle:'italic',
+        textAlign:'right',
+    },
+    inner_container:{
+       padding:5, 
+    },
+});
